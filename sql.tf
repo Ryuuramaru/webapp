212d2baa6e94b121e952ce323825676a38c99cc6
Ryuuramaru <rosumateisc@gmail.com>
2023-08-07 18:24:34 +0300

    Created a GCP SQL Database and API Keys

---
+++ sql.tf
@@ -0,0 +1,16 @@
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  region           = "europe-west3"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
